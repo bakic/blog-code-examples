5d91ceabe8a5af08e6e08d9428ea829bc7117cbd
Abdelbaki BEN ELHAJ SLIMENE <belhaj.abdelbaki@gmail.com>
2024-05-12 11:57:09 +0200

    Initial commit of openapi documentation

---
+++ openapi-documentation/src/main/java/com/baki/openapidoc/OpenapiDocApplication.java
@@ -0,0 +1,13 @@
+package com.baki.openapidoc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OpenapiDocApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OpenapiDocApplication.class, args);
+	}
+
+}
---
+++ openapi-documentation/src/test/java/com/baki/openapidoc/OpenapiDocApplicationTests.java
@@ -0,0 +1,13 @@
+package com.baki.openapidoc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OpenapiDocApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
